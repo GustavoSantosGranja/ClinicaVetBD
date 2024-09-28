c9f4e81744f41fd9ca1d30ecd43e563e1ad83e7d
GUSTAVO SANTOS GRANJA <44396027893@info.local>
2024-09-27 21:41:38 -0300

    ~ClinicaVetBD~

--- DML - ClinicaVetBD.sql
+++ DML - ClinicaVetBD.sql
@@ -1,94 +1,38 @@
 INSERT INTO Veterinarios (nome, especialidade, telefone) VALUES
-('Dr. João Silva', 
-'Cirurgia', 
-'1111-1111'
-);
+('Dr. João Silva', 'Cirurgia', '1111-1111');
 
 INSERT INTO Veterinarios (nome, especialidade, telefone) VALUES
-(
-'Dra. Maria Souza', 
-'Dermatologia', 
-'2222-2222'
-);
+('Dra. Maria Souza', 'Dermatologia', '2222-2222');
 
 INSERT INTO Veterinarios (nome, especialidade, telefone) VALUES
-(
-'Dr. Carlos Lima', 
-'Cardiologia', 
-'3333-3333'
-);
+('Dr. Carlos Lima', 'Cardiologia', '3333-3333');
 
 INSERT INTO Clientes (nome, endereco, telefone) VALUES
-(
-'Ana Pereira', 
-'Rua A, 123', 
-'4444-4444'
-);
+('Ana Pereira', 'Rua A, 123', '4444-4444');
 
 INSERT INTO Clientes (nome, endereco, telefone) VALUES
-(
-'Bruno Rocha', 
-'Rua B, 456', '
-5555-5555'
-);
+('Bruno Rocha', '5555-5555');
 
 INSERT INTO Clientes (nome, endereco, telefone) VALUES
-(
-'Carlos Mendes', 
-'Rua C, 789', 
-'6666-6666'
-);
+('Carlos Mendes', 'Rua C, 789', '6666-6666');
 
 INSERT INTO Pets (nome, tipo, raca, data_nascimento, id_cliente) VALUES
-(
-'Rex', 
-'Cachorro', 
-'Labrador', 
-'2018-06-01', 
-1
-);
+('Rex', 'Cachorro', 'Labrador', '2018-06-01', 1);
 
 INSERT INTO Pets (nome, tipo, raca, data_nascimento, id_cliente) VALUES
-(
-'Mia', 
-'Gato', 
-'Siamês',
-'2020-08-10', 
-2
-);
+('Mia', 'Gato', 'Siamês','2020-08-10', 2);
 
 INSERT INTO Pets (nome, tipo, raca, data_nascimento, id_cliente) VALUES
-(
-'Thor', 
-'Cachorro', 
-'Poodle', 
-'2015-01-15', 
-3
-);
+('Thor', 'Cachorro', 'Poodle', '2015-01-15', 3);
 
 INSERT INTO Atendimentos (id_pet, id_veterinario, data_atendimento, descricao) VALUES
-(
-1, 
-1, 
-'2024-01-10', 
-'Consulta de rotina'
-);
+(1, 1, '2024-01-10', 'Consulta de rotina');
 
 INSERT INTO Atendimentos (id_pet, id_veterinario, data_atendimento, descricao) VALUES
-(
-2, 
-2, 
-'2024-01-15', 
-'Problemas de pele'
-);
+(2, 2, '2024-01-15', 'Problemas de pele');
 
 INSERT INTO Atendimentos (id_pet, id_veterinario, data_atendimento, descricao) VALUES
-(
-3, 
-3, 
-'2024-01-20', 
-'Exame cardíaco'
-);
+(3, 3, '2024-01-20', 'Exame cardíaco');
 
 
 SELECT nome, data_nascimento 
